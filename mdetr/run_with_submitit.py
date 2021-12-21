7735f8a13f1e73b930d1dca30dc7aa5a684febfa
rstrudel <rstrudel@gmail.com>
2021-12-21 11:52:50 +0100

    submitit: update

--- mdetr/run_with_submitit.py
+++ mdetr/run_with_submitit.py
@@ -23,12 +23,28 @@ def parse_args():
     detection_parser = detection.get_args_parser()
     parser = argparse.ArgumentParser("Submitit detection", parents=[detection_parser])
 
-    parser.add_argument("--partition", default=None, type=str, help="Partition where to submit")
-    parser.add_argument("--ngpus", default=8, type=int, help="Number of gpus to request on each node")
-    parser.add_argument("--nodes", default=4, type=int, help="Number of nodes to request")
+    parser.add_argument(
+        "--constraint", default="", type=str, help="Partition where to submit"
+    )
+    parser.add_argument(
+        "--qos", default="qos_gpu-t3", type=str, help="Quality of service"
+    )
+    parser.add_argument(
+        "--ngpus", default=8, type=int, help="Number of gpus to request on each node"
+    )
+    parser.add_argument(
+        "--nodes", default=4, type=int, help="Number of nodes to request"
+    )
     parser.add_argument("--timeout", default=1200, type=int, help="Duration of the job")
-    parser.add_argument("--job_dir", default="", type=str, help="Job dir. Leave empty for automatic.")
-    parser.add_argument("--mail", default="", type=str, help="Email this user when the job finishes if specified")
+    parser.add_argument(
+        "--job_dir", default="", type=str, help="Job dir. Leave empty for automatic."
+    )
+    parser.add_argument(
+        "--mail",
+        default="",
+        type=str,
+        help="Email this user when the job finishes if specified",
+    )
     return parser.parse_args()
 
 
@@ -64,7 +80,11 @@ def grid_parameters(grid: Dict):
         yield dict(zip(grid.keys(), p))
 
 
-def sweep(executor: submitit.Executor, args: argparse.ArgumentParser, hyper_parameters: Iterable):
+def sweep(
+    executor: submitit.Executor,
+    args: argparse.ArgumentParser,
+    hyper_parameters: Iterable,
+):
     jobs = []
     with executor.batch():
         for grid_data in hyper_parameters:
@@ -88,7 +108,9 @@ class Trainer(object):
         import os
 
         os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
-        socket_name = os.popen("ip r | grep default | awk '{print $5}'").read().strip("\n")
+        socket_name = (
+            os.popen("ip r | grep default | awk '{print $5}'").read().strip("\n")
+        )
         print("Setting GLOO and NCCL sockets IFNAME to: {}".format(socket_name))
         os.environ["GLOO_SOCKET_IFNAME"] = socket_name
         os.environ["MDETR_CPU_REDUCE"] = "1"
@@ -118,7 +140,9 @@ class Trainer(object):
         import submitit
 
         job_env = submitit.JobEnvironment()
-        self.args.output_dir = Path(str(self.args.output_dir).replace("%j", str(job_env.job_id)))
+        self.args.output_dir = Path(
+            str(self.args.output_dir).replace("%j", str(job_env.job_id))
+        )
         self.args.gpu = job_env.local_rank
         self.args.rank = job_env.global_rank
         self.args.world_size = job_env.num_tasks
@@ -137,11 +161,10 @@ def main():
     # cluster setup is defined by environment variables
     num_gpus_per_node = args.ngpus
     nodes = args.nodes
-    partition = args.partition
+    constraint = args.constraint
+    qos = args.qos
     timeout_min = args.timeout
     kwargs = {}
-    if partition is not None:
-        kwargs["slurm_partition"] = partition
 
     executor.update_parameters(
         mem_gb=16 * num_gpus_per_node,
@@ -152,12 +175,16 @@ def main():
         timeout_min=timeout_min,  # max is 60 * 72
         # Below are cluster dependent parameters
         slurm_signal_delay_s=120,
+        qos=qos,
+        slurm_constraint=constraint,
         **kwargs,
     )
 
     executor.update_parameters(name="detectransformer")
     if args.mail:
-        executor.update_parameters(additional_parameters={"mail-user": args.mail, "mail-type": "END"})
+        executor.update_parameters(
+            additional_parameters={"mail-user": args.mail, "mail-type": "END"}
+        )
 
     args.dist_url = get_init_file(args).as_uri()
     args.output_dir = args.job_dir
