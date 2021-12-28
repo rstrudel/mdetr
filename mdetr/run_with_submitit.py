b39a8d3913506535a520a23ac76e9f6c421f1752
rstrudel <rstrudel@gmail.com>
2021-12-28 18:32:49 +0100

    submitit: 40go mem per gpu

--- mdetr/run_with_submitit.py
+++ mdetr/run_with_submitit.py
@@ -167,7 +167,7 @@ def main():
     kwargs = {}
 
     executor.update_parameters(
-        mem_gb=16 * num_gpus_per_node,
+        mem_gb=40 * num_gpus_per_node,
         gpus_per_node=num_gpus_per_node,
         tasks_per_node=num_gpus_per_node,  # one task per GPU
         cpus_per_task=10,
